ff101b039740569dff2e32616ec503aad40b7ec3
Marcos Mingo <marcosperga@gmail.com>
2023-05-20 22:08:51 -0300

    subida de primeros archivos

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,8 @@
+// ¿Qué valor devuelve su programa si los números son 2 *17 * 6?
+
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+multiplicacion = num1 * num2 * num3;
+
+console.log (multiplicacion);
---
+++ practico0/resta.js
@@ -0,0 +1,7 @@
+// ¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?
+
+let num1 = 10;
+let num2 = 2;
+let resta = num1 - num2;
+
+console.log (num1 - num2);
